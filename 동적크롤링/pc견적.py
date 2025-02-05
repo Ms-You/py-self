6a2bf2944bb3baf57042eb0bde3e699701f4c11c
Ms-You <msu122005@hs.ac.kr>
2025-02-05 17:44:49 +0900

    selenium을 이용한 pc 견적 - cpu 제품 목록 조회

--- 동적크롤링/pc견적.py
+++ 동적크롤링/pc견적.py
@@ -47,6 +47,17 @@ def choose_one(text, options):
     choose = input("-> ")
     return int(choose) - 1
 
+def parse_product():
+    products = []
+    for p in visibility_elements("table.tbl_list tbody tr[class^=productList_]"):
+        try:
+            name = p.find_element(By.CSS_SELECTOR, "p.subject a").text
+            price = p.find_element(By.CSS_SELECTOR, "span.prod_price").text
+        except:
+            continue
+        products.append((name, price))
+    return products
+
 category = {
     "cpu": "873",
     "메인보드": "875",
@@ -69,11 +80,8 @@ cpu_button.click()
 time.sleep(0.5)
 
 # CPU 제조사 가져오기
-manufacturer_list = []
 manufacturer_elements = visibility_elements_browser("div.search_option_item:nth-child(1) ul.search_cate_list li.search_cate_item span.item_text")
-
-for element in manufacturer_elements:
-    manufacturer_list.append(element.text)  # 제조사 이름 추가
+manufacturer_list = [element.text for element in manufacturer_elements]
 
 # 제조사 목록이 비어있지 않은지 확인
 if not manufacturer_list:
@@ -88,28 +96,36 @@ else:
     browser.execute_script("arguments[0].click();", manufacturer_checkbox)
 
 
-# 제조사에 따른 cpu 종류 가져오기
-if chosen_manufacturer == "인텔":
-    # 더보기 버튼
-    more_button = visibility_element("div.search_option_item:nth-child(2) button.btn_item_more")
-    browser.execute_script("arguments[0].click();", more_button)
-    time.sleep(1)
-    # cpu 종류
-    manufacturers_cpu_elements = visibility_elements_browser("div.search_option_item:nth-child(2) ul.search_cate_list li.search_cate_item span.item_text")
-elif chosen_manufacturer == "AMD":
-    more_button = visibility_element("div.search_option_item:nth-child(2) button.btn_item_more")
-    browser.execute_script("arguments[0].click();", more_button)
-    time.sleep(1)
-    manufacturers_cpu_elements = visibility_elements_browser("div.search_option_item:nth-child(3) ul.search_cate_list li.search_cate_item span.item_text")
+    # 제조사에 따른 cpu 종류 가져오기
+    if chosen_manufacturer == "인텔":
+        # 더보기 버튼
+        more_button = visibility_element("div.search_option_item:nth-child(2) button.btn_item_more")
+        browser.execute_script("arguments[0].click();", more_button)
+        time.sleep(1)
+        # cpu 종류
+        manufacturers_cpu_elements = visibility_elements_browser("div.search_option_item:nth-child(2) ul.search_cate_list li.search_cate_item span.item_text")
+    elif chosen_manufacturer == "AMD":
+        more_button = visibility_element("div.search_option_item:nth-child(3) button.btn_item_more")
+        browser.execute_script("arguments[0].click();", more_button)
+        time.sleep(1)
+        manufacturers_cpu_elements = visibility_elements_browser("div.search_option_item:nth-child(3) ul.search_cate_list li.search_cate_item span.item_text")
 
-# CPU 목록이 비어있는지 확인
-if not manufacturers_cpu_elements:
-    print(f"{chosen_manufacturer}에 대한 CPU 목록을 가져오는 데 실패했습니다.")
-else:
-    cpu_list = [element.text for element in manufacturers_cpu_elements]
-    chosen_index = choose_one(f"{chosen_manufacturer} CPU 종류를 선택해주세요.", cpu_list)
-    chosen_cpu = cpu_list[chosen_index]
-    print(f"선택한 CPU: {chosen_cpu}")
+    # cpu 목록이 비어있는지 확인
+    if not manufacturers_cpu_elements:
+        print(f"{chosen_manufacturer}에 대한 CPU 목록을 가져오는 데 실패했습니다.")
+    else:
+        cpu_list = [element.text for element in manufacturers_cpu_elements]
+        chosen_index = choose_one(f"{chosen_manufacturer} CPU 종류를 선택해주세요.", cpu_list)
+        chosen_cpu = cpu_list[chosen_index] + " " # 뒤에 공백이 추가되어 있음
+        print(f"선택한 CPU: {chosen_cpu}")
+
+        # cpu 종류 체크박스 클릭
+        cpu_checkbox = visibility_element_browser(f"input[data='{chosen_cpu}']")
+        browser.execute_script("arguments[0].click();", cpu_checkbox)
+
+        products = parse_product()
+        for p in products:
+            print(p)
 
 time.sleep(3)
 
